0dadffd7c00f7dde35cf32f0183c8df068f74de1
Rick Vermeil <rickv85@gmail.com>
2022-09-20 15:09:02 -0600

    Add reassignments and console logs

--- variables.js
+++ variables.js
@@ -4,3 +4,13 @@ var age = 37
 var zipCode = 80516
 var oldMan = true
 var isStudent = true
+
+oldMan = false
+age = 27
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(zipCode)
+console.log(oldMan)
+console.log(isStudent)
